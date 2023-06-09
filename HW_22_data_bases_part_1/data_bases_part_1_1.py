15041381e145b4e6130357c247c3d90899fb94c6
Tinidrill <126487649+Tinidrill@users.noreply.github.com>
2023-06-09 17:04:30 +0200

    Update HW_22_data_bases_part_1/data_bases_part_1_1.py
    Co-authored-by: Vitalii <webasman@gmail.com>

--- HW_22_data_bases_part_1/data_bases_part_1_1.py
+++ HW_22_data_bases_part_1/data_bases_part_1_1.py
@@ -6,7 +6,7 @@ cursor = conn.cursor()
 # from console
 
 --SELECT * FROM users;
---SELECT * FROM users WHERE age > 30;
+--SELECT COUNT(id) FROM users WHERE age > 30;
 --SELECT age, COUNT(age) as users FROM users WHERE age > 30 GROUP BY age;
 --SELECT age, COUNT(age) as users FROM users WHERE age > 30 GROUP BY age ORDER BY users DESC;
 --SELECT age, COUNT(age) as users FROM users WHERE age > 30 GROUP BY age HAVING users > 1 ORDER BY users DESC;
